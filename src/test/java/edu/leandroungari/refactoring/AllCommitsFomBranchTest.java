5d089292f3c89df04a5d83f2388e645d24577adc
Leandro Ungari <leandroungari@gmail.com>
2018-09-06 23:10:40 -0300

    Adding test

---
+++ src/test/java/edu/leandroungari/refactoring/AllCommitsFomBranchTest.java
@@ -0,0 +1,30 @@
+package edu.leandroungari.refactoring;
+
+import edu.leandroungari.refactoring.git.Branch;
+import edu.leandroungari.refactoring.git.Commit;
+import edu.leandroungari.refactoring.git.GitRepository;
+
+/**
+ * Hello world!
+ *
+ */
+public class AllCommitsFomBranchTest {
+	
+    public static void main(String[] args) throws Exception {
+    	
+        
+    	GitRepository rep = new GitRepository("https://github.com/junit-team/junit5.git", "/home/leandroungari/teste-junit-5");
+        
+        for (Branch branch: rep.getBranches()) {
+        	
+        	System.out.println("Branch name: " + branch.getName());
+        	
+        	for (Commit commit: rep.getCommits(branch)) {
+        		
+        		System.out.println(commit);
+        	}
+        }	
+        
+        
+    }
+}
---
+++ src/test/java/edu/leandroungari/refactoring/RefactoringsFromCommitTest.java
@@ -0,0 +1,32 @@
+package edu.leandroungari.refactoring;
+
+import java.util.ArrayList;
+
+import edu.leandroungari.refactoring.git.Branch;
+import edu.leandroungari.refactoring.git.Commit;
+import edu.leandroungari.refactoring.git.GitRepository;
+
+public class RefactoringsFromCommitTest {
+
+	public static void main(String[] args) throws Exception {
+
+		GitRepository rep = new GitRepository("https://github.com/refdiff-data/clojure.git",
+				"/home/leandroungari/teste-clojure");
+
+		for (Branch branch : rep.getBranches()) {
+
+			System.out.println("Branch name: " + branch.getName());
+			
+			ArrayList<Commit> commits = rep.getCommits(branch);
+			
+			/*System.out.println("Number of commits: " + commits.size());
+			for (Commit commit : commits) {
+
+				//System.out.println(commit);
+			}*/
+			
+			System.out.println(commits.get(0));
+		}
+
+	}
+}
